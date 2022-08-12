d471d272ee27fbaecc2c3ba61dbce0a12647d6a4
Niamh <niamh.brockbank@gmail.com>
2022-08-12 14:01:27 +0100

    Make format prettier

--- src/components/NewTaskSection.tsx
+++ src/components/NewTaskSection.tsx
@@ -1,37 +1,35 @@
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 export default function NewTaskSection(): JSX.Element {
-    const [taskTitle, setTaskTitle] = useState('')
-    const [taskBody, setTaskBody] = useState('')
-    
-    function addNewTask() {
-        const newTask = {
-            title : taskTitle,
-            body : taskBody
-        }
-        axios.post('https://to-do-app-nb.herokuapp.com/new/task', newTask)
-        setTaskTitle('')
-        setTaskBody('')
-    }
-  
-    return (
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskBody, setTaskBody] = useState("");
+
+  function addNewTask() {
+    const newTask = {
+      title: taskTitle,
+      body: taskBody,
+    };
+    axios.post("https://to-do-app-nb.herokuapp.com/new/task", newTask);
+    setTaskTitle("");
+    setTaskBody("");
+  }
+
+  return (
     <>
-      <input 
+      <input
         type="text"
-        placeholder="Title of task" 
+        placeholder="Title of task"
         value={taskTitle}
         onChange={(e) => setTaskTitle(e.target.value)}
       />
-      <input 
-        type="text" 
-        placeholder="Body of task" 
+      <input
+        type="text"
+        placeholder="Body of task"
         value={taskBody}
         onChange={(e) => setTaskBody(e.target.value)}
       />
-      <button onClick={addNewTask}>
-        Add new task
-      </button>
+      <button onClick={addNewTask}>Add new task</button>
     </>
   );
 }
