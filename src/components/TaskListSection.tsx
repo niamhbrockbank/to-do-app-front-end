2bded1fe79300bab28599cc1c3302eaadb7b02e3
Niamh <niamh.brockbank@gmail.com>
2022-08-25 17:40:31 +0100

    Move sortTaskList to utils

--- src/components/TaskListSection.tsx
+++ src/components/TaskListSection.tsx
@@ -1,6 +1,7 @@
 import Task from "./Task";
 import { useEffect, useState } from "react";
 import { ITaskWithId } from "../types";
+import sortTaskList from "../utils/sortTaskList";
 
 export default function TaskListSection(): JSX.Element {
   const [taskList, setTaskList] = useState<ITaskWithId[]>([]);
@@ -13,21 +14,6 @@ export default function TaskListSection(): JSX.Element {
     }
     getTaskList();
   }, []);
-
-  function sortDateDesc(task1: ITaskWithId, task2: ITaskWithId) {
-    if (task1.date_created < task2.date_created) {
-      return -1;
-    } else if (task1.date_created > task2.date_created) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
-  function sortTaskList(taskList: ITaskWithId[]): ITaskWithId[] {
-    const sortedTaskList = taskList.sort(sortDateDesc);
-    return sortedTaskList;
-  }
 
   const sortedTaskList = sortTaskList(taskList);
 
---
+++ src/utils/sortTaskList.ts
@@ -0,0 +1,16 @@
+import { ITaskWithId } from "../types";
+
+export default function sortTaskList(taskList: ITaskWithId[]): ITaskWithId[] {
+    const sortedTaskList = taskList.sort(sortDateDesc);
+    return sortedTaskList;
+  }
+
+  function sortDateDesc(task1: ITaskWithId, task2: ITaskWithId) {
+    if (task1.date_created < task2.date_created) {
+      return -1;
+    } else if (task1.date_created > task2.date_created) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
