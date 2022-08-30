d9f2efb53ac833c5c5eea2ea6dc76a2880bee7ef
Niamh <niamh.brockbank@gmail.com>
2022-08-30 12:50:51 +0100

    Add search bar with two-way binding

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function FilterSection(): JSX.Element {
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  function handleSearchChange(e : React.ChangeEvent<HTMLInputElement>): void{
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <nav id="filter_section">
-      ...Filtering capabilities go here...
-      <button className="filter_button">filter does not much</button>
+      <input 
+        id= 'search_bar' 
+        type="text" 
+        placeholder='Search for tasks here...' 
+        value={searchTerm} 
+        onChange={(e) => handleSearchChange(e)}/>
+      <button className="filter_button">this filter does not do much</button>
     </nav>
   );
 }
